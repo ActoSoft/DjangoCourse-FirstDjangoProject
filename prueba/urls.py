c328b29b80b259d8e4978d4ed265b468194e6649
Martín Melo <martin.melo.dev.97@gmail.com>
2020-07-03 19:44:06 -0500

    Image config done

--- prueba/urls.py
+++ prueba/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from movies import urls as movies_urls
 from users import urls as users_urls
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include(movies_urls, namespace='movies')),
     path('users/', include(users_urls, namespace='users'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
